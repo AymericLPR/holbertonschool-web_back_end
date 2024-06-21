b8ac4bd49c9cc8a1a419319a857a4ed5ec156a30
Aymeric <mric31400@gmail.com>
2024-06-21 13:07:52 +0000

    session auth file

--- Session_authentication/api/v1/auth/auth.py
+++ Session_authentication/api/v1/auth/auth.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
-""" Auth
+""" Module of Authentication
 """
-
 from flask import request
 from typing import List, TypeVar
+from os import getenv
 
 
 class Auth:
-    """ Auth class to manage API authentication """
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ authentication is required """
-        if path is None or excluded_paths is None or not excluded_paths:
-            return True
+    """ Class to manage the API authentication """
 
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Method for requiring authentication """
+        if path is None or excluded_paths is None or not len(excluded_paths):
+            return True
         if path[-1] != '/':
             path += '/'
         if excluded_paths[-1] != '/':
@@ -22,12 +22,20 @@ class Auth:
         return True
 
     def authorization_header(self, request=None) -> str:
-        """ authorization header """
+        """ Method that handles authorization header """
         if request is None:
             return None
 
         return request.headers.get("Authorization", None)
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ Method to get the current user """
+        """ Validates current user """
         return None
+
+    def session_cookie(self, request=None):
+        """ Return cookie value from request. """
+        if request is None:
+            return None
+
+        cookie_key = getenv('SESSION_NAME')
+        return request.cookies.get(cookie_key)
