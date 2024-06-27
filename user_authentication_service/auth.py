8d2b44e3ddcf0b69b5da32cabac38304af9a6c6f
Aymeric <mric31400@gmail.com>
2024-06-27 08:15:17 +0000

    task 16

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -74,7 +74,7 @@ class Auth:
             return None
 
 
-     def get_reset_password_token(self, email: str) -> str:
+    def get_reset_password_token(self, email: str) -> str:
         """ reset token """
         try:
             user = self._db.find_user_by(email=email)
