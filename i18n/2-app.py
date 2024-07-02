8d4adea5b57d66a79dec88ec53ef6f1b651e4916
Aymeric <mric31400@gmail.com>
2024-07-02 08:43:04 +0000

    try task 2

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -1,33 +1,42 @@
 #!/usr/bin/env python3
-""" Basic Flask app """
-from flask import Flask, render_template, g, request
+"""
+Basic Babel setup
+"""
+
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 app = Flask(__name__)
 babel = Babel(app)
 
 
-class Config:
-    """ Config """
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+class Config(object):
+    """
+    Config class for Babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app.config.from_object(Config)
+app.config.from_object("1-app.Config")
 
 
 @babel.localeselector
 def get_locale():
-    """ locale selector """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    """
+    Get the current locale
+    """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
-@app.route('/')
-def root():
-    """ basic Flask app """
-    return render_template('2-index.html')
+@app.route("/")
+def index():
+    """
+    function that render index.html template
+    """
+    return render_template("1-index.html")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
